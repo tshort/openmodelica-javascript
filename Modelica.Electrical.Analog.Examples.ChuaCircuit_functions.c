3cd71d5191949b3d37a55b131aa4ed363d5e3c1c
tshort <tshort.rlists@gmail.com>
2013-09-21 12:27:09 -0400

    Initial check in.

---
+++ Modelica.Electrical.Analog.Examples.ChuaCircuit_functions.c
@@ -0,0 +1,29 @@
+#include "Modelica.Electrical.Analog.Examples.ChuaCircuit_functions.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define _OMC_LIT0_data "'p"
+static const size_t _OMC_LIT0_strlen = 2;
+static const char _OMC_LIT0[3] = _OMC_LIT0_data;
+#define _OMC_LIT1_data "'p/s"
+static const size_t _OMC_LIT1_strlen = 4;
+static const char _OMC_LIT1[5] = _OMC_LIT1_data;
+#define _OMC_LIT2_data "C2.i / C2.C because C2.C == 0: File: /usr/lib/omlibrary/Modelica 3.2.1/Electrical/Analog/Basic.mo Line: 286"
+static const size_t _OMC_LIT2_strlen = 107;
+static const char _OMC_LIT2[108] = _OMC_LIT2_data;
+#define _OMC_LIT3_data "C1.i / C1.C because C1.C == 0: File: /usr/lib/omlibrary/Modelica 3.2.1/Electrical/Analog/Basic.mo Line: 286"
+static const size_t _OMC_LIT3_strlen = 107;
+static const char _OMC_LIT3[108] = _OMC_LIT3_data;
+#define _OMC_LIT4_data "L.v / L.L because L.L == 0: File: /usr/lib/omlibrary/Modelica 3.2.1/Electrical/Analog/Basic.mo Line: 338"
+static const size_t _OMC_LIT4_strlen = 104;
+static const char _OMC_LIT4[105] = _OMC_LIT4_data;
+#define _OMC_LIT5_data "G.G / (1.0 + G.alpha * (G.T - G.T_ref)) because 1.0 + G.alpha * (G.T - G.T_ref) == 0: File: /usr/lib/omlibrary/Modelica 3.2.1/Electrical/Analog/Basic.mo Line: 223"
+static const size_t _OMC_LIT5_strlen = 162;
+static const char _OMC_LIT5[163] = _OMC_LIT5_data;
+
+#ifdef __cplusplus
+}
+#endif
+
